ce3973afc8725820da167a10c303792245561f2e
JeromeHtz <jerome.hertzog@epita.fr>
2022-04-13 11:42:38 +0200

    initialisation - fonctions basiques listes

--- listes.ml
+++ listes.ml
@@ -45,3 +45,53 @@ let retourner liste =
       [] -> res
     | e::l -> return l (e::res) 
   in return liste [];;
+
+(* tri par insertion *)
+let insert x list =
+  let rec insert_rec list booleen = match (list,booleen) with
+      ([],true) -> []
+    | ([],false) -> x::[]
+    | (e::l,b) when e>x && b=false -> x::e::insert_rec l (true)
+    | (e::l,b) -> e::insert_rec l b 
+  in insert_rec list false;;
+
+let insertion_sort list = 
+  let rec insertion_rec list res = match list with
+      [] -> res
+    | e::l -> let res = insert e res in insertion_rec l res 
+  in insertion_rec list [];;
+
+(* tri par selection *)
+let get_max list =
+  let e::rest = list in
+    let rec max elt liste = match liste with
+        [] -> elt
+      | e::l when e>elt -> max e l
+      | e::l -> max elt l
+     in max e rest ;;
+
+let remove x liste = 
+  let rec remove_rec liste res booleen = match (liste,res,booleen) with
+      ([],_,false) -> invalid_arg "remove : impossible de supprimer"
+    | ([],r,true) -> r
+    | (e::l,r,b) when x=e -> remove_rec l r (true)
+    | (e::l,r,b) -> e::remove_rec l r b 
+  in remove_rec liste [] false;;
+
+let selection_sort list = 
+  let rec selection_sort_rec liste res = match liste with
+      [] -> res
+    | liste -> let max = get_max liste in selection_sort_rec (remove max liste)(max::res)
+  in selection_sort_rec list [];;
+
+(* insertion d'une valeur dans une liste de listes *)
+let rec insert_nth (x,y) v list = match (x,list) with
+    (x,_) when x<0 -> invalid_arg "insert_nth : invalid index"
+  | (0,[]) -> []
+  | (x,[]) -> [v]::[]
+  | (0,e::li) -> let rec insert y liste = match (y,liste) with
+              (0,l) -> v::l
+            | (y,[]) -> v::[]
+            | (y,e::l) -> e::insert(y-1) l 
+          in insert y e :: insert_nth (0,y) v li 
+  | (x,e::li) -> e::insert_nth (x-1,y) v li;;
